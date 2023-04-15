25b951e68cb800816d774cad6ab663b1384ec5ef
kmtGryffindor20 <129510465+kmtGryffindor20@users.noreply.github.com>
2023-04-16 01:39:47 +0530

    added css and js files to the static folder

---
+++ static/script.js
@@ -0,0 +1,52 @@
+
+let slideIndex = 1;
+var x = window.matchMedia("(max-width: 600px)")
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+let i;
+let slides = document.getElementsByClassName("book");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+}
+if(x.matches)
+{
+    slides[slideIndex-1].style.display = "flex";
+    slides[slideIndex-1].style.flexDirection = "column";
+}
+else{
+    slides[slideIndex-1].style.display = "block";
+}
+}
+
+
+let menuBar = document.getElementsByClassName("nav-links");
+
+menuBar[0].style.maxHeight = "0px";
+function toggleMenu(){
+    let menuBar = document.getElementsByClassName("nav-links");
+    let image = document.getElementById("menu");
+    if(menuBar[0].style.maxHeight == "250px")
+    {
+        menuBar[0].style.maxHeight = "0px";
+        image.src = "../static/images/hamburger-icon.png";
+    }
+    else
+    {
+        menuBar[0].style.maxHeight = "250px";
+        image.src = "../static/images/cross-icon.png";
+    }
+    
+}
