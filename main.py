a19ba9884fa4e137d76d84e7de3d1de58b8d99e4
kmtGryffindor20 <129510465+kmtGryffindor20@users.noreply.github.com>
2023-04-17 00:10:35 +0530

    Add DataBase, Login and User system

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import json
 import random
 from xml.dom import ValidationErr
 import requests
-from flask import Flask, redirect, render_template, request
+from flask import Flask, flash, redirect, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField
-# import sqlite3
+from wtforms.validators import *
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user 
 import os
 
 
@@ -14,26 +15,103 @@ app = Flask(__name__)
 
 app.secret_key = "abcd"
 
-#creating the DB
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://users.db'
-
-#initializing DB
-
-# db = SQLAlchemy(app)
-
-# class Users(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(250), nullable = False)
-#     email = db.Column(db.String(250), nullable=False, unique=True)
-
-#     #create a string
-
-#     def __repr__(self):
-#         return '<Name %r>' % self.name
-    
+# creating the DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 
 
 
+# Login Manager
+
+login_manger = LoginManager()
+login_manger.init_app(app)
+login_manger.login_view = 'login'
+
+
+    # initializing DB
+
+db = SQLAlchemy(app)
+
+class Users(db.Model, UserMixin):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(250), nullable = False)
+        email = db.Column(db.String(250), nullable=False, unique=True)
+        password = db.Column(db.String(250), nullable = False)
+        genres = db.Column(db.String(250), nullable=True)
+        authors = db.Column(db.String(250), nullable=True)
+        #create a string
+
+        def __repr__(self):
+            return '<Name %r>' % self.name
+
+with app.app_context():
+        
+    db.create_all()
+
+
+@login_manger.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+
+# class UserForm(FlaskForm):
+#     name = StringField("Username", validators=[DataRequired()])
+#     email = StringField("Email", validators=[DataRequired()])
+#     password = PasswordField("Password", validators=[DataRequired()])
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == "POST":
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        repeat_password = request.form['rep-pass']
+
+        if repeat_password != password:
+            flash("Passwords don't match")
+        else:
+            user = Users.query.filter_by(email=email)
+
+            try:
+                user = Users(username=username, email=email, password=password)
+                db.session.add(user)
+                db.session.commit()
+                flash("User Added")
+            except:
+                flash("User Already Exists. Login Instead")
+
+            return redirect('/login')
+
+
+    return render_template('sign_up.html')
+
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        user = Users.query.filter_by(username=request.form['username']).first()
+        if user:
+            #check pass
+            if user.password == request.form['password']:
+                login_user(user)
+                flash("Login Successful")
+                form = SearchForm()
+                return redirect('/')
+            else:
+                flash("Wrong Password - Try Again")
+        else:
+            flash("User does not exists. Register Now")
+    return render_template('login.html')
+
+
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out")
+    return redirect("/login")
 
 
 
@@ -99,13 +177,14 @@ def get_data():
         validate_search(form, form)
         if query != "":
             data = search_query(query, 40)
-            all_books = []
+            volumeInfos = []
             for item in data['items']:
-                all_books.append(item['volumeInfo']['title'])
-                if 'imageLinks' not in item['volumeInfo']:
-                    item['volumeInfo']['imageLinks'] = '#'
-                if 'authors' not in item['volumeInfo']:
-                    item['volumeInfo']['authors'] = '#'
+                if 'averageRating' not in item['volumeInfo']:
+                    item['volumeInfo']['averageRating'] = 0
+                volumeInfos.append(item['volumeInfo'])
+    
+            data = sorted(volumeInfos, key=lambda d: d['averageRating'], reverse=True) 
+
             return render_template('search.html', search = query, data = data)
         else:
             return render_template('index.html', form=form)
@@ -127,34 +206,40 @@ def get_book(book_name, id):
     except KeyError:
         book['volumeInfo']['imageLinks'] = "#"
 
-    authors=book['volumeInfo']['authors']
+    genres = ""
+    if 'categories' in book['volumeInfo']:
+        genres = book['volumeInfo']['categories'][0]
+    authors = ""
+    if 'authors' in book['volumeInfo']:
+        authors = book['volumeInfo']['authors'][0]
 
-    try:
-        genres = {"genre":book['volumeInfo']['categories'], "author":authors}
-    except KeyError:
-        genres = {'genre':""}
-
-    try:
-        with open("genres.json", "r") as file:
-            genre_data = json.load(file)
-    except FileNotFoundError:
-        with open("genres.json", "w") as file:
-            json.dump(genres, file, indent=4)
+    
+    id = current_user.id
+    user_to_update = Users.query.get_or_404(id)
+    user_genres = user_to_update.genres
+    if user_genres == None:
+        user_genres = genres
     else:
-        for genre in genres['genre']:
-            genre_data['genre'].append(genre)
+        if len(user_genres) < 230:
+            user_genres += f" {genres}"
+    user_to_update.genres = user_genres
 
-        if 'author' in genres:
-            for author in genres['author']:
-                genre_data['author'].append(author)
+    user_authors = user_to_update.authors
+    if user_authors == None:
+        user_authors = authors
+    else:
+        if len(user_authors) < 230:
+            user_authors += f" {authors}"
+    user_to_update.authors = user_authors
 
+    db.session.commit()
+    
 
+    # hasAverageRating='averageRating' in book['volumeInfo']
+    # if not hasAverageRating:
+    #     book['volumeInfo']['averageRating'] = ""
 
-        with open("genres.json", "w") as file:
-            json.dump(genre_data, file, indent=4)
-
-    hasAverageRating='averageRating' in book['volumeInfo']
-    if not hasAverageRating:
+    if 'averageRating' not in book['volumeInfo']:
         book['volumeInfo']['averageRating'] = ""
 
     if 'imageLinks' in book['volumeInfo']:
@@ -179,39 +264,6 @@ def get_book(book_name, id):
     flipkart = f"https://www.flipkart.com/search?q={book_name}+{book['volumeInfo']['authors'][0]}"
     amazon = f"https://www.amazon.in/s?k={book_name}+{book['volumeInfo']['authors'][0]}"
 
-    # if request.method == "POST":
-    #     isFav = request.form.fav
-    #     isFav = isFav.value()
-
-    #     if isFav == "0":
-    #         return redirect('book.html', 
-    #                        book_name=book_name,  
-    #                        amazon_link = amazon, 
-    #                        flipkart_link=flipkart,
-    #                        info=book['volumeInfo']['infoLink'],
-    #                        thumbnail=thumbnail,
-    #                        author=book['volumeInfo']['authors'][0],
-    #                        year=year,
-    #                        rating=book['volumeInfo']['averageRating'],
-    #                        description=description,
-    #                        id = id,
-    #                        fav_icon="../static/images/fav-no-fill.png")
-    #     else:
-    #         return redirect('book.html', 
-    #                        book_name=book_name,  
-    #                        amazon_link = amazon, 
-    #                        flipkart_link=flipkart,
-    #                        info=book['volumeInfo']['infoLink'],
-    #                        thumbnail=thumbnail,
-    #                        author=book['volumeInfo']['authors'][0],
-    #                        year=year,
-    #                        rating=book['volumeInfo']['averageRating'],
-    #                        description=description,
-    #                        id = id,
-    #                        fav_icon="../static/images/fav-fill.png")
-
-
-
     return render_template('book.html', 
                            book_name=book_name,  
                            amazon_link = amazon, 
@@ -222,20 +274,19 @@ def get_book(book_name, id):
                            year=year,
                            rating=book['volumeInfo']['averageRating'],
                            description=description,
-                           id = id,
-                           fav_icon="../static/images/fav-no-fill.png")
+                           id = id)
 
 
 
 @app.route("/recommend")
 def recommend():
+    id = current_user.id
+    user_data = Users.query.get_or_404(id)
     try:
-        with open("genres.json", "r") as file:
-            genre_data = json.load(file)
-            author_data = genre_data['author']
-            genre_data = genre_data['genre']
-           
-    except FileNotFoundError:
+        genre_data = user_data.genres.split()
+        author_data = user_data.genres.split()
+
+    except:
         authors = ["JK Rowling", "Stephen King", "Paulo Coelho", "Suzanne Collins", "Rick Riordan", "Chetan Bhagat",
                    "Amish Tripathi", "Ernest Hemingway", "Agatha Christie"]
         data_genre = search_query("fiction", 10)
@@ -243,9 +294,9 @@ def recommend():
         data_author2 = search_query(f"author:{random.choice(authors)}", 10)
 
     else:
-        most_common_authors = max(set(author_data), key=author_data.count)
+        most_common_author = max(set(author_data), key=author_data.count)
         for author in author_data:
-            if author == most_common_authors:
+            if author == most_common_author:
                 author_data.remove(author)
         if len(author_data) > 0:
             most_common_author2 = max(set(author_data), key=author_data.count)
@@ -253,15 +304,25 @@ def recommend():
             most_common_author2 = ""
 
         most_common_genre = max(set(genre_data), key=genre_data.count)
+
         data_genre = search_query(f"subject:{most_common_genre}", 10)
-        data_author = search_query(f"author:{most_common_authors}", 10)
+        data_author = search_query(f"author:{most_common_author}", 10)
         data_author2 = search_query(f"author:{most_common_author2}", 10)
+
     data = data_genre
     for item in data_author['items']:
         data['items'].append(item)
     for item in data_author2['items']:
         data['items'].append(item)
-    random.shuffle(data['items'])
+
+    volumeInfos = []
+    for item in data['items']:
+        if 'averageRating' not in item['volumeInfo']:
+            item['volumeInfo']['averageRating'] = 0
+        volumeInfos.append(item['volumeInfo'])
+    
+    data = sorted(volumeInfos, key=lambda d: d['averageRating'], reverse=True) 
+
     return render_template('search.html', search = "Recommendation", data = data)
 
 
