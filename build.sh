babde58e29ad1dbb9e580e4a45b1d4a49d4e7593
kmtGryffindor20 <129510465+kmtGryffindor20@users.noreply.github.com>
2023-05-11 19:47:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,1 @@
-pip install --upgrade pip
-pip freeze > requirements.txt
 pip install -r requirements.txt
-pip install requests
-pip install flask
-pip install Flask-WTF
-pip install Flask-SQLAlchemy
-pip install Flask-Login
-pip install gunicorn
